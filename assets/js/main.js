82f0b9381b3f7660a61ffac280cef78fdb885120
NEVI0 <neviocostamagagnin@gmail.com>
2022-03-23 21:12:45 -0300

    feat: adding new template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,36 @@
+// SHOW MENU
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+// REMOVE MOBILE MENU
+const navLinks = document.querySelectorAll('.nav__link');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+});
+
 // ADD BOX-SHADOW IN SCROLLING DOWN IN THE HEADER
 window.addEventListener('scroll', () => {
     const header = document.getElementById('header');
 
-    if (this.scrollY >= 80) {
+    if (this.scrollY >= 40) {
         header.classList.add('scroll-header');
     } else {
         header.classList.remove('scroll-header');
     }
-});
-
-// SCROLL TO
-const scrollUp = document.getElementById('scroll-up');
-scrollUp.addEventListener('click', () => {
-    window.scrollTo({ top: 0 });
-});
-
-// SHOW SCROLL TO TOP BUTTON
-window.addEventListener('scroll', () => {
-    if (this.scrollY <= 200) {
-        scrollUp.classList.add('show-scroll');
-    } else {
-        scrollUp.classList.remove('show-scroll');
-    }
-});
-
-// CHANGE THEME
-const themeButton = document.getElementById('change-theme');
-themeButton.addEventListener('click', () => {
-
-    const themeIcon = document.getElementById('change-theme-icon');
-    const currentTheme = localStorage.getItem('@PORTFOLIO:theme');
-
-    if (currentTheme === 'dark-theme') {
-        document.body.classList.replace('dark-theme', 'light-theme');
-        themeIcon.classList.replace('ri-sun-line', 'ri-moon-line');
-        localStorage.setItem('@PORTFOLIO:theme', 'light-theme');
-    } else {
-        document.body.classList.replace('light-theme', 'dark-theme');
-        themeIcon.classList.replace('ri-moon-line', 'ri-sun-line');
-        localStorage.setItem('@PORTFOLIO:theme', 'dark-theme');
-    }
-
 });
